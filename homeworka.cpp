b60d1721dd8e907a74cd25a2280719d04ab4db45
Napach Nuibanphot <67995922+geogiez@users.noreply.github.com>
2020-07-08 14:23:47 +0700

    Add files via upload

---
+++ homeworka.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+void main()
+{
+	
+	cout<< "    a     "<<endl;
+	cout<< "   a a    "<<endl;
+	cout<< "  a   a   "<<endl;
+	cout<< " a     a  "<<endl;
+	cout<< " aaaaaaa  "<<endl;
+	cout<< " a     a  "<<endl;
+	cout<<endl;
+	cout<<"Napach nuibanphot";
+	cout<<endl;
+}
+  
